5b5644dec0f1d1c6790795035becf8de1d36139a
eternalsad <sadikcartman@gmail.com>
2018-06-11 22:46:23 +0600

    Starting tracking changes in stats.js file

---
+++ js/stats.js
@@ -0,0 +1,5 @@
+window.renderStatistics = function (ctx, names, times) {
+    ctx.fillStyle = 'rgba(256, 256, 256, 1.0)'; // make color of rect white
+    ctx.strokeRect(100, 10, 420, 270);
+    сtx.fillRect(100, 10, 420, 270);
+};
