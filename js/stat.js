7b4c7137647116e8447043ba450e79750fee2e25
eternalsad <sadikcartman@gmail.com>
2018-06-12 00:04:47 +0600

    fixed ctx not defined bug

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,16 @@
+'use strict';
 window.renderStatistics = function (ctx, names, times) {
-    ctx.fillStyle = 'rgba(256, 256, 256, 1.0)'; // make color of rect white
+    ctx.fillStyle ='rgba(0, 0, 0, 0.7)';
+    ctx.strokeRect(110, 20, 420, 270);
+    ctx.fillRect(110, 20, 420, 270);
+
+    ctx.fillStyle = 'white'; // make color of rect white
     ctx.strokeRect(100, 10, 420, 270);
-    сtx.fillRect(100, 10, 420, 270);
+    ctx.fillRect(100, 10, 420, 270);
+
+    ctx.fillStyle = '#000';
+    ctx.font = '14px PT Mono';
+
+    ctx.fillText('Вы победили', 120, 40);
+
 };
